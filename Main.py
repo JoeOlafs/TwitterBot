7d62e5100c9de30b1726a8e1258248b406f0e276
JoeOlafs <jaolafsson@hotmail.com>
2021-02-18 12:21:58 +0200

    finish twitter bot - without automatic running

--- Main.py
+++ Main.py
@@ -12,13 +12,13 @@ with open('day.txt', 'r') as file:
      day = file.read()
 dayNum = int(day)
 nextDay = dayNum + 1
-#day = day.replace(day, str(nextDay))
+day = day.replace(day, str(nextDay))
 
 GitContribution = info.contributionToday
-#print(GitContribution)
 
 tweet =   f"#Day{dayNum} of #100Days of Code Challenge\nI made {GitContribution} contributions to my Github profile today\nSent out with my #TwitterBot\n#freeCodeCamp @freeCodeCamp @_100DaysOfCode"
-print(tweet)
 
-#with open('day.txt', 'w') as file:
-#     file.write(day)
+api.PostUpdate(tweet)
+
+with open('day.txt', 'w') as file:
+     file.write(day)

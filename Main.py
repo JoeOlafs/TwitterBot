e7650f0060b606b6febcca6082d8bfe403924489
JoeOlafs <jaolafsson@hotmail.com>
2021-02-18 12:30:15 +0200

    added if no contributions made

--- Main.py
+++ Main.py
@@ -11,14 +11,18 @@ api = twitter.Api(consumer_key=os.environ.get('TwitterAPIKey'),
 with open('day.txt', 'r') as file:
      day = file.read()
 dayNum = int(day)
-nextDay = dayNum + 1
-day = day.replace(day, str(nextDay))
 
 GitContribution = info.contributionToday
-
-tweet =   f"#Day{dayNum} of #100Days of Code Challenge\nI made {GitContribution} contributions to my Github profile today\nSent out with my #TwitterBot\n#freeCodeCamp @freeCodeCamp @_100DaysOfCode"
+if GitContribution == 0:
+     nextDay = 1
+     tweet = f"No contributions made to my Github profile today, guess I'll start #100DaysOfCodeAgain tomorrow @freeCodeCamp #freeCodeCamp @_100DaysOfCode"
+else:
+     nextDay = dayNum + 1
+     day = day.replace(day, str(nextDay))
+     tweet =   f"#Day{dayNum} of #100DaysofCodeChallenge\nI made {GitContribution} contributions to my Github profile today. Sent out with my #TwitterBot\n#freeCodeCamp @freeCodeCamp @_100DaysOfCode"
 
 api.PostUpdate(tweet)
+
 
 with open('day.txt', 'w') as file:
      file.write(day)

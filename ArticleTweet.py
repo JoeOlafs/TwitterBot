d8789af436f85c07ac0328b6713d7124f53dc2fc
JoeOlafs <jaolafsson@hotmail.com>
2021-02-25 19:52:51 +0200

    Still Stuck in the same ****** loop

--- ArticleTweet.py
+++ ArticleTweet.py
@@ -12,13 +12,16 @@ while i < len(ArticleInfo.links):
      pot_link = ArticleInfo.links[i]
      pot_link2 = ArticleInfo.links[i] + '\\n'
      print(pot_link2)
-     if pot_link not in used_links and pot_link2 not in used_links:
+     if pot_link in used_links:
+          print(pot_link + ' has been used')
+          i = i + 1
+     elif pot_link2 in used_links:
+          print(pot_link + ' has been used')
+          i = i + 1
+     else:
           next_link = pot_link
           with open('links.txt', 'a') as file:
                file.write('\n')
                file.write(next_link)
           print(next_link)
           break
-     else:
-          print(pot_link + ' has been used')
-          i = i + 1

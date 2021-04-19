af546d01a08749dab26b36f3a0c913f13e4e2194
JoeOlafs <jaolafsson@hotmail.com>
2021-04-19 19:53:42 +0300

    minor update

--- Run.py
+++ Run.py
@@ -3,14 +3,15 @@ import time
 import Main
 
 while True:
+     print('TwitterBot is running')
+     print('Next tweet will be at 23:55')
      try:
           now = datetime.now()
           curr_time = now.strftime("%H:%M:%S")
           var = curr_time.split(':')
           hour = var[0]
           minute = var[1]
-          print('TwitterBot is running')
-          print('Next tweet will be at 23:55')
+          
           if int(hour) == 23 and int(minute) == 55:
                Main.git_tweet()
                print('Git tweet posted')
